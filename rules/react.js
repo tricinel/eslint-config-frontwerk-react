2c44ffe1c9ca2eac9d9d54b4fcbae7b4f9d291bf
Bogdan Lazar <bogdan@bogdanlazar.com>
2019-10-10 12:47:06 +0200

    Add base react rules

--- rules/react.js
+++ rules/react.js
@@ -5,5 +5,272 @@ module.exports = {
       jsx: true
     }
   },
-  rules: {}
+  rules: {
+    // Require that boolean props begin with "is" or "has"
+    // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/boolean-prop-naming.md
+    'react/boolean-prop-naming': [
+      'error',
+      { rule: '^(is|has)[A-Z]([A-Za-z0-9]?)+' }
+    ],
+
+    // Require that <button> elements have an explicit "type"
+    // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/button-has-type.md
+    'react/button-has-type': 'error',
+
+    // Require that any "defaultProp" has a non-required "PropType" declaration
+    // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/default-props-match-prop-types.md
+    'react/default-props-match-prop-types': [
+      'error',
+      { allowRequiredDefaults: true }
+    ],
+
+    // Warn when not using destructuring assignment for props, state and context
+    // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/destructuring-assignment.md
+    'react/destructuring-assignment': ['warn', 'always'],
+
+    // Require that you use displayName to name components
+    // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/display-name.md
+    'react/display-name': 'error',
+
+    // Disallow certain props on components, such as "className" and "style"
+    // Let's not force developers into this
+    // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/forbid-component-props.md
+    'react/forbid-component-props': 'off',
+
+    // Disallow certain props on native DOM elements
+    // Let's not force developers into this
+    // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/forbid-dom-props.md
+    'react/forbid-dom-props': 'off',
+
+    // Disallow certain elements in favor of others
+    // Let's not force developers into this
+    // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/forbid-elements.md
+    'react/forbid-elements': 'off',
+
+    // Disallow certain prop types, like "any", "array" and "object"
+    // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/forbid-prop-types.md
+    'react/forbid-prop-types': [
+      'error',
+      {
+        forbid: ['any', 'array', 'object'],
+        checkContextTypes: true,
+        checkChildContextTypes: true
+      }
+    ],
+
+    // Disallow using another component's prop types unless they are explicitly imported/exported
+    // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/forbid-foreign-prop-types.md
+    'react/forbid-foreign-prop-types': 'error',
+
+    // Disallow using this.state within this.setState
+    // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/no-access-state-in-setstate.md
+    'react/no-access-state-in-setstate': 'error',
+
+    // Disallow using an Array index for the element key, e.g. in a map
+    // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/no-array-index-key.md
+    'react/no-array-index-key': 'error',
+
+    // Disallow passing children as a prop
+    // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/no-children-prop.md
+    'react/no-children-prop': 'error',
+
+    // Warn when using "dangerouslySetInnerHTML"
+    // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/no-danger.md
+    'react/no-danger': 'warn',
+
+    // Disallow using children and the "dangerouslySetInnerHTML" prop at the same time
+    // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/no-danger-with-children.md
+    'react/no-danger-with-children': 'error',
+
+    // Disallow using deprecated methods in React
+    // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/no-deprecated.md
+    'react/no-deprecated': 'error',
+
+    // Allow setting state in "componentDidMount" because it is required for SSR
+    // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/no-did-mount-set-state.md
+    'react/no-did-mount-set-state': 'off',
+
+    // Disallow setting state in "componentDidUpdate" because it will cause a second "render()"
+    // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/no-did-update-set-state.md
+    'react/no-did-update-set-state': 'error',
+
+    // Disallow setting state in "componentWillUpdate"
+    // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/no-will-update-set-state.md
+    'react/no-will-update-set-state': 'error',
+
+    // Disallow directly mutating the state instead of using "setState"
+    // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/no-direct-mutation-state.md
+    'react/no-direct-mutation-state': 'error',
+
+    // Disallow using "findDOMNode" and use callback refs instead
+    // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/no-find-dom-node.md
+    'react/no-find-dom-node': 'error',
+
+    // Disallow using "isMounted"
+    // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/no-is-mounted.md
+    'react/no-is-mounted': 'error',
+
+    // Allow declaring multiple components per file
+    // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/no-multi-comp.md
+    'react/no-multi-comp': 'off',
+
+    // Disallow "shouldComponentUpdate" when extending "PureComponent"
+    // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/no-redundant-should-component-update.md
+    'react/no-redundant-should-component-update': 'error',
+
+    // Disallow using the reference to the root component return from "ReactDOM.render()"
+    // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/no-render-return-value.md
+    'react/no-render-return-value': 'error',
+
+    // Allow the use of local component state
+    // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/no-set-state.md
+    'react/no-set-state': 'off',
+
+    // Ensure that class properties as well as the lifecycle methods have no casing typos
+    // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/no-typos.md
+    'react/no-typos': 'error',
+
+    // Disallow using a string identifier to refer to components
+    // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/no-string-refs.md
+    'react/no-string-refs': 'error',
+
+    // Disallow using "this" when using a stateless functional component
+    // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/no-this-in-sfc.md
+    'react/no-this-in-sfc': 'error',
+
+    // Prevent characters that should have been escaped from appearing in the markup
+    // e.g. ">" instead of "&gt;" with <Foo foo="bar">>Baz</Foo>
+    // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/no-unescaped-entities.md
+    'react/no-unescaped-entities': 'error',
+
+    // Disallow using unknown DOM property and make sure they are camelCased, e.g. className
+    // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/no-unknown-property.md
+    'react/no-unknown-property': 'error',
+
+    // Disallow using legacy methods marked as "UNSAFE_" and their aliases
+    // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/no-unsafe.md
+    'react/no-unsafe': ['error', { checkAliases: true }],
+
+    // Require that all declared propTypes be used in the component
+    // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/no-unused-prop-types.md
+    'react/no-unused-prop-types': 'error',
+
+    // Require that all declared properties on the state be used in the component
+    // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/no-unused-state.md
+    'react/no-unused-state': 'error',
+
+    // Require that React Components be created using ES6 classes instead of "create-react-class"
+    // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/prefer-es6-class.md
+    'react/prefer-es6-class': 'error',
+
+    // Enforce that props defined using Flow are read-only
+    // Developers using Flow should enable this
+    // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/prefer-read-only-props.md
+    'react/prefer-read-only-props': 'off',
+
+    // Require React Components be written as pure functions wherever possible
+    // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/prefer-stateless-function.md
+    'react/prefer-stateless-function': 'error',
+
+    // Require that props have PropTypes
+    // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/prop-types.md
+    'react/prop-types': 'error',
+
+    // Require that React be imported/required when using JSX
+    // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/react-in-jsx-scope.md
+    'react/react-in-jsx-scope': 'error',
+
+    // Require that any non-required PropType declaration of a component has a corresponding defaultProps value
+    // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/require-default-props.md
+    'react/require-default-props': [
+      'error',
+      {
+        forbidDefaultForRequired: true
+      }
+    ],
+
+    // Allow creating React components without declaring a "shouldComponentUpdate" method
+    // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/require-optimization.md
+    'react/require-optimization': 'off',
+
+    // Require using the "return" statement in a "render" method
+    // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/require-render-return.md
+    'react/require-render-return': 'error',
+
+    // Require that components without children are self-closed
+    // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/self-closing-comp.md
+    'react/self-closing-comp': 'error',
+
+    // Require that component definition follow the same organisation for method order
+    // Taken from airbnb's (https://github.com/airbnb/javascript/blob/master/packages/eslint-config-airbnb/rules/react.js)
+    // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/sort-comp.md
+    'react/sort-comp': [
+      'error',
+      {
+        order: [
+          'static-variables',
+          'static-methods',
+          'instance-variables',
+          'lifecycle',
+          '/^on.+$/',
+          'getters',
+          'setters',
+          '/^(get|set)(?!(InitialState$|DefaultProps$|ChildContext$)).+$/',
+          'instance-methods',
+          'everything-else',
+          'rendering'
+        ],
+        groups: {
+          lifecycle: [
+            'displayName',
+            'propTypes',
+            'contextTypes',
+            'childContextTypes',
+            'mixins',
+            'statics',
+            'defaultProps',
+            'constructor',
+            'getDefaultProps',
+            'getInitialState',
+            'state',
+            'getChildContext',
+            'getDerivedStateFromProps',
+            'componentWillMount',
+            'UNSAFE_componentWillMount',
+            'componentDidMount',
+            'componentWillReceiveProps',
+            'UNSAFE_componentWillReceiveProps',
+            'shouldComponentUpdate',
+            'componentWillUpdate',
+            'UNSAFE_componentWillUpdate',
+            'getSnapshotBeforeUpdate',
+            'componentDidUpdate',
+            'componentDidCatch',
+            'componentWillUnmount'
+          ],
+          rendering: ['/^render.+$/', 'render']
+        }
+      }
+    ],
+
+    // Allow any order for prop types
+    // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/sort-prop-types.md
+    'react/sort-prop-types': 'off',
+
+    // Require that state initialisation be with a class property
+    // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/state-in-constructor.md
+    'react/state-in-constructor': ['error', 'never'],
+
+    // Require that static properties be defined as static public fields
+    // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/static-property-placement.md
+    'react/static-property-placement': ['error', 'static public field'],
+
+    // Require that the style prop value be an object or a variable that is an object
+    // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/style-prop-object.md
+    'react/style-prop-object': 'error',
+
+    // Disallow setting children to self-closing DOM elements, like <img> <br> or <hr>
+    // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/void-dom-elements-no-children.md
+    'react/void-dom-elements-no-children': 'error'
+  }
 };
